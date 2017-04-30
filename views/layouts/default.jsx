aa4267c8b43a928184c19dc4c08ce375d131b969
Jen <jtrudell@gmail.com>
2017-04-30 11:43:25 -0500

    fix typo

--- views/layouts/default.jsx
+++ views/layouts/default.jsx
@@ -13,11 +13,10 @@ class DefaultLayout extends React.Component {
         <body>
           {this.props.children}
           <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-          <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>=
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
         </body>
       </html>
     );
   }
 }
 module.exports = DefaultLayout;
-
